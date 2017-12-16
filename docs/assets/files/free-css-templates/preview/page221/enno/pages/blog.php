30a7200ae32c92c185d857169a1f4e876bfc8ea4
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2017-12-16 20:52:22 +0800

    Create blog.php

---
+++ docs/assets/files/free-css-templates/preview/page221/enno/pages/blog.php
@@ -0,0 +1,237 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Free CSS | Free CSS Templates | Demo of the HTML CSS Template Enno | Pages | Blog</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/animate.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/jquery.bxslider.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/overwrite.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/normalize.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/demo.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/set1.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+<!--[if lt IE 9]>
+<script src="../assets/js/html5shiv.min.js"></script>
+<script src="../assets/js/respond.min.js"></script>
+<![endif]-->
+<style type="text/css">
+#freecssfooter{display:block;width:100%;padding:120px 0 20px;overflow:hidden;background-color:transparent;z-index:5000;text-align:center;}
+#freecssfooter div#fcssholder div{display:none;}
+#freecssfooter div#fcssholder div:first-child{display:block;}
+#freecssfooter div#fcssholder div:first-child a{float:none;margin:0 auto;}
+</style></head>
+<body>
+<script type="text/javascript">
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = '//s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
+</script>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+      <a class="navbar-brand" href="https://websitebloger.github.io/free-css-temp/"><span>eNno</span></a></div>
+    <div class="navbar-collapse collapse">
+      <div class="menu">
+        <ul class="nav nav-tabs" role="tablist">
+          <li role="presentation" class="active"><a href="https://websitebloger.github.io/free-css-temp/">Home</a></li>
+          <li role="presentation"><a href="services.php">Services</a></li>
+          <li role="presentation"><a href="blog.php">Blog</a></li>
+          <li role="presentation"><a href="portfolio.php">Portfolio</a></li>
+          <li role="presentation"><a href="contact.php">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="blogs">
+        <div class="text-center">
+          <h2>Blog</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit Cras suscipit arcu<br>
+            vestibulum volutpat libero sollicitudin vitae Curabitur ac aliquam <br>
+          </p>
+        </div>
+        <hr>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+      <div class="page-header">
+        <div class="blog">
+          <h5>February,22 2015</h5>
+          <img src="../assets/img/01.jpg" class="img-responsive" alt="website template image">
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+          <h3>Lorem ipsum dolor sit amet</h3>
+          <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.</p>
+          <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+          <div class="ficon"><a href="https://websitebloger.github.io/free-css-temp/">Learn more</a> </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <nav>
+            <ul class="pagination">
+              <li><a href="https://websitebloger.github.io/free-css-temp/"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>
+              <li><a href="https://websitebloger.github.io/free-css-temp/">1</a></li>
+              <li><a href="https://websitebloger.github.io/free-css-temp/">2</a></li>
+              <li><a href="https://websitebloger.github.io/free-css-temp/">3</a></li>
+              <li><a href="https://websitebloger.github.io/free-css-temp/">4</a></li>
+              <li><a href="https://websitebloger.github.io/free-css-temp/">5</a></li>
+              <li><a href="https://websitebloger.github.io/free-css-temp/"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <form action="#" method="post" class="form-search">
+        <input class="form-control" type="text" placeholder="Search..">
+      </form>
+      <div class="panel panel-default">
+        <div class="panel-heading"><strong>Popular Posts</strong></div>
+        <div class="panel-body">
+          <div class="media"><a class="media-left" href="https://websitebloger.github.io/free-css-temp/"><img src="../assets/img/b.jpg" alt="website template image"></a>
+            <div class="media-body">
+              <h4 class="media-heading">Kelly Hidayah</h4>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore </p>
+              <div class="ficon"><a href="https://websitebloger.github.io/free-css-temp/">Read more</a> </div>
+            </div>
+          </div>
+        </div>
+        <div class="panel-body">
+          <div class="media"><a class="media-left" href="https://websitebloger.github.io/free-css-temp/"><img src="../assets/img/a.jpg" alt="website template image"></a>
+            <div class="media-body">
+              <h4 class="media-heading">Kelly Hidayah</h4>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore </p>
+              <div class="ficon"><a href="https://websitebloger.github.io/free-css-temp/">Read more</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="panel-body">
+          <div class="media"><a class="media-left" href="https://websitebloger.github.io/free-css-temp/"><img src="../assets/img/c.jpg" alt="website template image"></a>
+            <div class="media-body">
+              <h4 class="media-heading">Kelly Hidayah</h4>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore </p>
+              <div class="ficon"><a href="https://websitebloger.github.io/free-css-temp/">Read more</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="panel-body">
+          <div class="media"><a class="media-left" href="https://websitebloger.github.io/free-css-temp/"><img src="../assets/img/d.jpg" alt="website template image"></a>
+            <div class="media-body">
+              <h4 class="media-heading">Kelly Hidayah</h4>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore </p>
+              <div class="ficon"><a href="https://websitebloger.github.io/free-css-temp/">Read more</a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+      <div class="embed-responsive embed-responsive-4by3">
+        <iframe width="560" height="315" src="//www.youtube.com/embed/HrdAkX0ue3k?list=PLB523918A978EF359" frameborder="1" allowfullscreen></iframe>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="popular-tags">
+        <h5>Popular tags</h5>
+        <ul class="tags">
+          <li><a href="https://websitebloger.github.io/free-css-temp/">Web design</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/">Trends</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/">Technology</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/">Internet</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/">Tutorial</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/">Development</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<footer>
+  <div class="inner-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 f-about"><a href="https://websitebloger.github.io/free-css-temp/">
+          <h1><span>e</span>Nno</h1>
+          </a>
+          <p>Lorem ipsum dolor sit amet consectetur adipiscing elit Cras suscipit arcu libero vestibulum volutpat libero sollicitudin vitae Curabitur ac aliquam  consectetur adipiscing elit Cras suscipit arcu libero</p>
+        </div>
+        <div class="col-md-4 l-posts">
+          <h3 class="widgetheading">Latest Posts</h3>
+          <ul>
+            <li><a href="https://websitebloger.github.io/free-css-temp/">This is awesome post title</a></li>
+            <li><a href="https://websitebloger.github.io/free-css-temp/">Awesome features are awesome</a></li>
+            <li><a href="https://websitebloger.github.io/free-css-temp/">Create your own awesome website</a></li>
+            <li><a href="https://websitebloger.github.io/free-css-temp/">Wow, this is fourth post title</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4 f-contact">
+          <h3 class="widgetheading">Stay in touch</h3>
+          <a href="https://websitebloger.github.io/free-css-temp/">
+          <p><i class="fa fa-envelope"></i> example@gmail.com</p>
+          </a>
+          <p><i class="fa fa-phone"></i> +345 578 59 45 416</p>
+          <p><i class="fa fa-home"></i> Enno inc  |  PO Box 23456, Little Lonsdale St, New York <br>
+            Victoria 8011 USA</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="last-div">
+    <div class="container">
+      <div class="row">
+        <div class="copyright">&copy; eNno Multi-purpose theme | <a target="_blank" rel="nofollow" href="http://bootstrapmade.com/">BootstrapMade</a></div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <ul class="social-network">
+          <li><a href="https://websitebloger.github.io/free-css-temp/" data-placement="top" title="Facebook"><i class="fa fa-facebook fa-1x"></i></a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/" data-placement="top" title="Twitter"><i class="fa fa-twitter fa-1x"></i></a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/" data-placement="top" title="Linkedin"><i class="fa fa-linkedin fa-1x"></i></a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/" data-placement="top" title="Pinterest"><i class="fa fa-pinterest fa-1x"></i></a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/" data-placement="top" title="Google plus"><i class="fa fa-google-plus fa-1x"></i></a></li>
+        </ul>
+      </div>
+    </div>
+    <a href="javascript:void(0)" class="scrollup"><i class="fa fa-chevron-up"></i></a></div>
+</footer>
+<script src="../assets/js/jquery-2.1.1.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src="../assets/js/wow.min.js"></script>
+<script src="../assets/js/jquery.easing.1.3.js"></script>
+<script src="../assets/js/jquery.isotope.min.js"></script>
+<script src="../assets/js/jquery.bxslider.min.js"></script>
+<script src="../assets/js/fliplightbox.min.js"></script>
+<script src="../assets/js/functions.js"></script>
+<script>$('.portfolio').flipLightBox()</script>
+<div id="freecssfooter">
+  <div id="fcssholder">
+    <div id="bsap_2365" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1635" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1279659" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+  </div>
+</div>
+<script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-72183449-1', 'auto');ga('send', 'pageview');</script>
+</body>
+</html>
