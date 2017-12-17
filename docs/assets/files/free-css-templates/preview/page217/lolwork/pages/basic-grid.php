8423230ee69f7eead1ee9499efa26ff239863acc
Marlon Deposoy <34613480+marlondeposoy@users.noreply.github.com>
2017-12-17 10:53:53 +0800

    Add files via upload

---
+++ docs/assets/files/free-css-templates/preview/page217/lolwork/pages/basic-grid.php
@@ -0,0 +1,198 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Free CSS | Free CSS Templates | Demo of the HTML CSS Template Lolwork | Pages | Basic Grid</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link rel="stylesheet" type="text/css" href="../assets/layout/styles/layout.css">
+<style type="text/css">
+#freecssfooter{display:block;width:100%;padding:120px 0 20px;overflow:hidden;background-color:transparent;z-index:5000;text-align:center;}
+#freecssfooter div#fcssholder div{display:none;}
+#freecssfooter div#fcssholder div:first-child{display:block;}
+#freecssfooter div#fcssholder div:first-child a{float:none;margin:0 auto;}
+</style></head>
+<body id="top">
+<script type="text/javascript">
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = '//s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
+</script>
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear">
+    <div class="fl_left">
+      <ul class="nospace inline pushright">
+        <li><i class="fa fa-sign-in"></i> <a href="https://websitebloger.github.io/free-css/free-css-templates">Login</a></li>
+        <li><i class="fa fa-user"></i> <a href="https://websitebloger.github.io/free-css/free-css-templates">Register</a></li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <form class="clear" method="post" action="#">
+        <fieldset>
+          <legend>Search:</legend>
+          <input type="search" value="" placeholder="Search Here&hellip;">
+          <button class="fa fa-search" type="submit" title="Search"><em>Search</em></button>
+        </fieldset>
+      </form>
+    </div>
+  </div>
+</div>
+<div class="wrapper row1">
+  <header id="header" class="hoc clear">
+    <div id="logo" class="fl_left">
+      <h1><a href="https://websitebloger.github.io/free-css/free-css-templates">Lolwork</a></h1>
+      <p>Fusce iaculis feugiat</p>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Home</a></li>
+        <li class="active"><a class="drop" href="https://websitebloger.github.io/free-css/free-css-templates">Pages</a>
+          <ul>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="full-width.php">Full Width</a></li>
+            <li><a href="sidebar-left.php">Sidebar Left</a></li>
+            <li><a href="sidebar-right.php">Sidebar Right</a></li>
+            <li class="active"><a href="basic-grid.php">Basic Grid</a></li>
+          </ul>
+        </li>
+        <li><a class="drop" href="https://websitebloger.github.io/free-css/free-css-templates">Dropdown</a>
+          <ul>
+            <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Level 2</a></li>
+            <li><a class="drop" href="https://websitebloger.github.io/free-css/free-css-templates">Level 2 + Drop</a>
+              <ul>
+                <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Level 3</a></li>
+                <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Level 3</a></li>
+                <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Level 3</a></li>
+              </ul>
+            </li>
+            <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Level 2</a></li>
+          </ul>
+        </li>
+        <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Link Text</a></li>
+        <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Link Text</a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
+<div class="wrapper row2">
+  <div id="breadcrumb" class="hoc clear">
+    <ul>
+      <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Home</a></li>
+      <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Lorem</a></li>
+      <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Ipsum</a></li>
+      <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Dolor</a></li>
+    </ul>
+  </div>
+</div>
+<div class="wrapper row3">
+  <main class="hoc container clear">
+    <div class="content">
+      <h2>Half</h2>
+      <div class="group btmspace-50 demo">
+        <div class="one_half first">1/2</div>
+        <div class="one_half">1/2</div>
+      </div>
+      <h2>Quarter</h2>
+      <div class="group btmspace-50 demo">
+        <div class="one_quarter first">1/4</div>
+        <div class="one_quarter">1/4</div>
+        <div class="one_quarter">1/4</div>
+        <div class="one_quarter">1/4</div>
+      </div>
+      <div class="group btmspace-50 demo">
+        <div class="one_quarter first">1/4</div>
+        <div class="one_quarter">1/4</div>
+        <div class="two_quarter">2/4 or 1/2</div>
+      </div>
+      <div class="group btmspace-50 demo">
+        <div class="one_quarter first">1/4</div>
+        <div class="three_quarter">3/4</div>
+      </div>
+      <h2>Third</h2>
+      <div class="group btmspace-50 demo">
+        <div class="one_third first">1/3</div>
+        <div class="one_third">1/3</div>
+        <div class="one_third">1/3</div>
+      </div>
+      <div class="group demo">
+        <div class="one_third first">1/3</div>
+        <div class="two_third">2/3</div>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </main>
+</div>
+<div class="wrapper row4 bgded overlay" style="background-image:url('../assets/images/demo/backgrounds/03.png');">
+  <footer id="footer" class="hoc clear">
+    <div class="one_quarter first">
+      <h6 class="title">Libero semper nec</h6>
+      <p>Eros et magna ut vitae faucibus dui nec fermentum mauris nunc molestie venenatis metus et porttitor nisi faucibus ut cras non sem facilisis.</p>
+      <p>Hendrerit ligula ut blandit sem nulla non ipsum rutrum tempor dolor non faucibus eros cras imperdiet cursus sapien.</p>
+    </div>
+    <div class="one_quarter">
+      <h6 class="title">Nisi ornare aliquet</h6>
+      <ul class="nospace linklist contact">
+        <li><i class="fa fa-map-marker"></i>
+          <address>
+          Street Name &amp; Number, Town, Postcode/Zip
+          </address>
+        </li>
+        <li><i class="fa fa-phone"></i> +00 (123) 456 7890<br>
+          +00 (123) 456 7890</li>
+        <li><i class="fa fa-fax"></i> +00 (123) 456 7890</li>
+        <li><i class="fa fa-envelope-o"></i> info@domain.com</li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="title">Donec at tellus eu</h6>
+      <ul class="nospace linklist">
+        <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Mauris in condimentum lacus</a></li>
+        <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Sed hendrerit est praesent</a></li>
+        <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Tempor dapibus enim phasellus</a></li>
+        <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Convallis lectus a tristique</a></li>
+        <li><a href="https://websitebloger.github.io/free-css/free-css-templates">Lectus nisi sodales sem at</a></li>
+      </ul>
+    </div>
+    <div class="one_quarter">
+      <h6 class="title">ac urna morbi rutrum</h6>
+      <ul class="nospace linklist">
+        <li>
+          <article>
+            <h2 class="nospace font-x1"><a href="https://websitebloger.github.io/free-css/free-css-templates">dignissim libero augue</a></h2>
+            <time class="font-xs block btmspace-10" datetime="2045-04-06">Friday, 6<sup>th</sup> April 2045</time>
+            <p class="nospace">aenean non egestas urna curabitur vitae aliquam felis&hellip;</p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <h2 class="nospace font-x1"><a href="https://websitebloger.github.io/free-css/free-css-templates">et nisl id pretium</a></h2>
+            <time class="font-xs block btmspace-10" datetime="2045-04-05">Thursday, 5<sup>th</sup> April 2045</time>
+            <p class="nospace">eros vehicula eros nec lacinia sem tellus vel turpis vivamus&hellip;</p>
+          </article>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</div>
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear">
+    <p class="fl_left">Copyright &copy; 2017 - All Rights Reserved - <a href="https://websitebloger.github.io/free-css/free-css-templates">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+  </div>
+</div>
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<script src="../assets/layout/scripts/jquery.min.js"></script>
+<div id="freecssfooter">
+  <div id="fcssholder">
+    <div id="bsap_2365" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1635" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1279659" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+  </div>
+</div>
+<script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-72183449-1', 'auto');ga('send', 'pageview');</script>
+</body>
+</html>
