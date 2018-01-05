699555dcd037a3cdc64626fdd13a0d8dffffa97c
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2018-01-05 12:41:04 +0800

    Update services.php

--- assets/files/premium-website-template/transform/pages/services.php
+++ assets/files/premium-website-template/transform/pages/services.php
@@ -337,6 +337,6 @@ Postcode/Zip
 <div id="bsap_1244497" class="bsarocks bsap_2cdb89802e2deca5991138bb3e47b146"></div>
 </div>
 </div>
-<script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','UA-23865188-1','auto');ga('send','pageview');</script>
+<script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','UA-72183449-1','auto');ga('send','pageview');</script>
 </body>
 </html>
