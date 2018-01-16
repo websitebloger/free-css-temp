52f717417e488382c09e3823049cba33987e3205
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2018-01-16 16:27:07 +0800

    Create blog.php

---
+++ assets/files/free-css-templates/preview/page222/lesser/pages/blog.php
@@ -0,0 +1,171 @@
+
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+<title>Free CSS | Free CSS Templates | Demo of the HTML CSS Template Lesser | Pages | Blog</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/plugins.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+<script src="../assets/js/modernizr-2.6.2.min.js"></script>
+<!--[if lt IE 9]>
+<script src="../assets/js/respond.min.js"></script>
+<![endif]-->
+<style type="text/css">
+#freecssfooter{display:block;width:100%;padding:120px 0 20px;overflow:hidden;background-color:transparent;z-index:5000;text-align:center;}
+#freecssfooter div#fcssholder div{display:none;}
+#freecssfooter div#fcssholder div:first-child{display:block;}
+#freecssfooter div#fcssholder div:first-child a{float:none;margin:0 auto;}
+</style></head>
+<body>
+<script type="text/javascript">
+(function(){
+  var bsa = document.createElement('script');
+     bsa.type = 'text/javascript';
+     bsa.async = true;
+     bsa.src = '//s3.buysellads.com/ac/bsa.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
+</script>
+<div id="fh5co-page">
+  <header id="fh5co-header" role="banner">
+    <div class="container">
+      <div class="header-inner">
+        <h1><i class="sl-icon-energy"></i><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Lesser</a></h1>
+        <nav role="navigation">
+          <ul>
+            <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Home</a></li>
+            <li><a class="active" href="blog.php">Blog</a></li>
+            <li><a href="portfolio.php">Portfolio</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+  <div id="fh5co-blog-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+          <h2>Our Blog</h2>
+          <p><span>Lorem ipsum dolor sit amet consectetur</span></p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 text-center">
+          <div class="blog-inner"><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><img class="img-responsive" src="../assets/images/image_4.jpg" alt="website template image"></a>
+            <div class="desc">
+              <h3><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">New iPhone 6 Released</a></h3>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+              <p><a href="https://websitebloger.github.io/free-css-temp/free-css-templates" class="btn btn-primary btn-outline with-arrow">Read More<i class="icon-arrow-right"></i></a></p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="blog-inner"><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><img class="img-responsive" src="../assets/images/image_7.jpg" alt="website template image"></a>
+                <div class="desc">
+                  <h3><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">New iPhone 6 Released</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                  <p><a href="https://websitebloger.github.io/free-css-temp/free-css-templates" class="btn btn-primary btn-outline with-arrow">Read More<i class="icon-arrow-right"></i></a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 text-center">
+          <div class="blog-inner"><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><img class="img-responsive" src="../assets/images/image_5.jpg" alt="website template image"></a>
+            <div class="desc">
+              <h3><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Beans</a></h3>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+              <p><a href="https://websitebloger.github.io/free-css-temp/free-css-templates" class="btn btn-primary btn-outline with-arrow">Read More<i class="icon-arrow-right"></i></a></p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="blog-inner"><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><img class="img-responsive" src="../assets/images/image_9.jpg" alt="website template image"></a>
+                <div class="desc">
+                  <h3><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">New iPhone 6 Released</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                  <p><a href="https://websitebloger.github.io/free-css-temp/free-css-templates" class="btn btn-primary btn-outline with-arrow">Read More<i class="icon-arrow-right"></i></a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 text-center">
+          <div class="blog-inner"><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><img class="img-responsive" src="../assets/images/image_6.jpg" alt="website template image"></a>
+            <div class="desc">
+              <h3><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Bookmarksgrove right</a></h3>
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+              <p><a href="https://websitebloger.github.io/free-css-temp/free-css-templates" class="btn btn-primary btn-outline with-arrow">Read More<i class="icon-arrow-right"></i></a></p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="blog-inner"><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><img class="img-responsive" src="../assets/images/image_10.jpg" alt="website template image"></a>
+                <div class="desc">
+                  <h3><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">New iPhone 6 Released</a></h3>
+                  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                  <p><a href="https://websitebloger.github.io/free-css-temp/free-css-templates" class="btn btn-primary btn-outline with-arrow">Read More<i class="icon-arrow-right"></i></a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer id="fh5co-footer" role="contentinfo">
+    <div class="container">
+      <div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+        <h3>About Us</h3>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+      </div>
+      <div class="col-md-6 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+        <h3>Our Services</h3>
+        <ul class="float">
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Web Design</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Branding &amp; Identity</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Free HTML5</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">HandCrafted Templates</a></li>
+        </ul>
+        <ul class="float">
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Free Bootstrap Template</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Free HTML5 Template</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">Free HTML5 &amp; CSS3 Template</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates">HandCrafted Templates</a></li>
+        </ul>
+      </div>
+      <div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+        <h3>Follow Us</h3>
+        <ul class="fh5co-social">
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><i class="icon-twitter"></i> Twitter</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><i class="icon-facebook"></i> Facebook</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><i class="icon-google-plus"></i> Google Plus</a></li>
+          <li><a href="https://websitebloger.github.io/free-css-temp/free-css-templates"><i class="icon-instagram"></i> Instagram</a></li>
+        </ul>
+      </div>
+      <div class="col-md-12 fh5co-copyright text-center">
+        <p>&copy; All Rights Reserved. <span>Designed with <i class="icon-heart"></i> by <a target="_blank" rel="nofollow" href="http://freehtml5.co/">FreeHTML5.co</a> Demo Images by Unsplash</span></p>
+      </div>
+    </div>
+  </footer>
+</div>
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/jquery.easing.1.3.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src="../assets/js/jquery.waypoints.min.js"></script>
+<script src="../assets/js/main.js"></script>
+<div id="freecssfooter">
+  <div id="fcssholder">
+    <div id="bsap_2365" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+    <div id="bsap_1635" class="bsarocks bsap_b893e54e42ad5b76e7b252f59be18e67"></div>
+  </div>
+</div>
+<script type="text/javascript">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-72183449-1', 'auto');ga('send', 'pageview');</script>
+</body>
+</html>
